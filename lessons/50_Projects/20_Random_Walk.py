146de7b0ed515fad411a4e8fc451a2008742a2c9
League Student <student@jointheleague.org>
2026-06-26 12:29:01 -0700

    Auto-commit: workspace changes before push

--- lessons/50_Projects/20_Random_Walk.py
+++
@@ -1,9 +0,0 @@
-def print_derpy_box():
-    face = "ʕっ•ᴥ•ʔっ"
-    border = "+" + "-" * (len(face) + 4) + "+"
-    
-    print(border)
-    print(f"|  {face}  |")
-    print(border)
-
-print_derpy_box()
